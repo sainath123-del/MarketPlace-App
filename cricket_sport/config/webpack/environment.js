6e678d57728ab9be480c175b68bf2b55b28d43f5
sainath biradar <ajay@192-168-1-113.tpgi.com.au>
2021-06-01 09:44:10 +1000

    Rails

--- cricket_sport/config/webpack/environment.js
+++ cricket_sport/config/webpack/environment.js
@@ -1,14 +1,14 @@
-const { environment } = require("@rails/webpacker");
+const { environment } = require('@rails/webpacker');
 
 environment.plugins.append("Provide",
-      new webpack.ProvidePlugin(
-    {
-        $: "jquery",
-        jQuery: "jquery",
-        Popper: ['popper.js', 'default'],
-    }
+    new webpack.ProvidePlugin(
+        {
+            $: "jquery",
+            jQuery: "jquery",
+            Popper: ['popper.js', 'default'],
+        }
 
-));
+    ));
 
 
-module.exports = environment
+module.exports = environment;
