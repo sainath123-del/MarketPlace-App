f3da9bd83157c12df0741191f8e549ba111c95dd
sainath biradar <ajay@192-168-1-113.tpgi.com.au>
2021-06-15 16:05:18 +1000

    Update of heroku

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,28 +38,28 @@ class ProductsController < ApplicationController
   end
 
   def show
-    stripe_session = Stripe: :Checkout: :Session.create(
-   payment_method_typs: ['card'],
-   client_reference_is: current_user.id,
-   customer_email: current_user.email,
-   line_items: [{ 
-     amount: (@product.price * 100).to_i,
-     name: @product.name,
-     description: @product.description,
-     currency: 'aud',
-     quantity: 1
-   }],
-   payment_intent_data: {
-     metadata: {
-       product_id: @product_id,
-       user_id: current_user.id
-     }
-   },
-   success_url: "#{root_url}/purchases/success?productId=#{@product.id}", 
-   cancel_url: "#{root_url}products"
-    )
+  #   stripe_session = Stripe::Checkout::Session.create(
+  #  payment_method_typs: ['card'],
+  #  client_reference_id: current_user ? current_user.id : nil,
+  #     customer_email: current_user ? current_user.email : nil,
+  #  line_items:[{ 
+  #    amount: (@product.price * 100).to_i,
+  #    name: @product.name,
+  #    description: @product.description,
+  #    currency: 'aud',
+  #    quantity: 1
+  #  }],
+  #  payment_intent_data: {
+  #    metadata: {
+  #      product_id: @product_id,
+  #      user_id: current_user ? current_user.id : nil
+  #     }
+  #  },
+  #  success_url: "#{root_url}/purchases/success?productId=#{@product.id}", 
+  #  cancel_url: "#{root_url}products"
+  #   )
 
-    @session_id = stripe_session.id
+  #   @session_id = stripe_session.id
 
   end
 
--- db/migrate/20210610065649_create_users.rb
+++ db/migrate/20210610065649_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    # create_table :users do |t|
-
-    #   t.timestamps
-    # end
+    create_table :users do |t|
+      t.string :name
+      t.text :email
+      t.timestamps
+    end
   end
 end
