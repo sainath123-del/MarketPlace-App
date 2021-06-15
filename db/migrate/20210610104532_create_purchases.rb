40c7be635bf3e4b7da56bf0217c843c8f5be108b
sainath biradar <ajay@192-168-1-113.tpgi.com.au>
2021-06-15 17:54:49 +1000

    Deploy

--- db/migrate/20210610104532_create_purchases.rb
+++ db/migrate/20210610104532_create_purchases.rb
@@ -2,7 +2,7 @@ class CreatePurchases < ActiveRecord::Migration[6.1]
   def change
     create_table :purchases do |t|
       t.references :product, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: true
       t.string :receipt_url
       t.string :payment_intent_id
 
